0c5cce4ade74912f652a0da4cf97a3a7c9354d24
Lucas <tuga@vivaldi.net>
2021-06-10 12:34:36 +0200

    fetch details done

--- views/Details.tsx
+++ views/Details.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
-export default function Details() {
+export default function Details({ route }: any) {
+
+    const [Product, SetProduct] = useState<Product | null>(null)
+    const { ProductId } = route.params
+
+    useEffect(() => {
+        getProduct()
+    }, [])
+
+    const getProduct = async () => {
+        const response = await axios.get<Product>("https://world.openfoodfacts.org/api/v0/product/" + ProductId)
+        const product: Product = response.data
+        SetProduct(product)
+    };
+
+
     return (
         <View>
-            <Text>Details View</Text>
+            {Product === null ?
+                <ActivityIndicator size="large" />
+                :
+                <Text>Details View :::: {Product.code}</Text>
+            }
         </View>
     )
 }
